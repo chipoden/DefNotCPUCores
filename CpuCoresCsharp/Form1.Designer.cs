6ff8a51211244dcae2428d85e50549e251c4c910
chipoden <46577899+chipoden@users.noreply.github.com>
2019-01-12 23:42:26 +0000

    Add files via upload

---
+++ CpuCoresCsharp/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace CpuCoresCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(117, 134);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(203, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 24);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Processes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Startup with windows";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(241, 210);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Yes";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(284, 210);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(37, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "No";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(241, 239);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(37, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Yes";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(284, 239);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(37, 23);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "No";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Manual Boost";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(524, 271);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CPUFixer V0.1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CpuCoresCsharp/Form1.cs
@@ -0,0 +1,500 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpuCoresCsharp
+{
+    public partial class Form1 : Form
+
+    {
+        int i = 2;
+        int capa = 1;
+        string[] processos = new string[100];
+        int count = 0;
+        int sim = 1;
+        int b = 2;
+        int ativar = 0;
+        string go;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+
+            while (b > 1)
+            {
+
+
+                for (i = 0; i < count; i++)
+                {
+
+
+                    int ativar = 0;
+
+                    sim = 1;
+                    //it checks if the processes are running
+                    System.Diagnostics.Process[] proc = System.Diagnostics.Process.GetProcessesByName(processos[i]);
+                    if (proc.Length > 0)
+                    {
+                      //  System.Threading.Thread.Sleep(5000);
+
+                        do
+                        {
+                            //System.Threading.Thread.Sleep(5000);
+                            if (ativar == 0) { 
+                            Process[] pa = Process.GetProcessesByName(processos[i]);
+
+                            foreach (Process process in pa)
+
+                            {
+                               // System.Threading.Thread.Sleep(10000);
+
+
+                                process.PriorityClass = ProcessPriorityClass.High;
+
+                            }
+
+                            Process[] allProc = Process.GetProcesses();
+                            Process me = Process.GetCurrentProcess();
+                           // System.Threading.Thread.Sleep(5000);
+                            foreach (Process p in Process.GetProcesses())
+                            {
+
+                                try
+                                {
+                                    if (p.Id != me.Id
+                                        && p.ProcessName != "winlogon"
+                                        && p.ProcessName != "explorer"
+                                        && p.ProcessName != "wininit"
+                                        && p.ProcessName != "System"
+                                        && p.ProcessName != "fontdrvhost"
+                                        && p.ProcessName != "winlogon"
+                                        && p.ProcessName != "SecurityHealthService"
+                                        && p.ProcessName != "Registry"
+                                        && p.ProcessName != "MsMpEng"
+                                        && p.ProcessName != "System Idle Process"
+                                        && p.ProcessName != "Processo inativo do sistema"
+                                        && p.ProcessName != "ShellExperienceHost"
+                                        && p.ProcessName != "RuntimeBroker"
+                                        && p.ProcessName != "taskmgr"
+                                        && p.ProcessName != "spoolsv"
+                                        && p.ProcessName != "csrss"
+                                        && p.ProcessName != "smss"
+                                        && p.ProcessName != "svchost"
+                                        && p.ProcessName != "services"
+                                        && p.ProcessName != "chrome"
+                                        && p.ProcessName != "discord"
+                                        && p.ProcessName != "Taskmgr"
+                                        && p.ProcessName != processos[i]
+                                    )
+                                    {
+                                        p.PriorityClass = ProcessPriorityClass.BelowNormal;
+                                        p.ProcessorAffinity = (IntPtr)0x0001;
+
+                                    }
+                                 //   System.Threading.Thread.Sleep(500);
+                                }
+                                catch (Exception ex)
+                                {
+                                    //logging goes here
+                                    continue;
+                                }
+                            }
+                            }
+                            ativar = 1;
+
+                            System.Diagnostics.Process[] skrr = System.Diagnostics.Process.GetProcessesByName(processos[i]);
+                            // if checks when the program is closed
+                            if (ativar == 1)
+                            { 
+                                if (skrr.Length > 0)
+                                {
+                                }
+                                else
+                                {
+
+                                    Process[] pa = Process.GetProcessesByName(processos[i]);
+                                    Process me = Process.GetCurrentProcess();
+
+
+
+                                    foreach (Process process in pa)
+
+                                    {
+
+                                        process.PriorityClass = ProcessPriorityClass.Normal;
+
+                                    }
+                                    foreach (Process p in Process.GetProcesses())
+                                    {
+                                        try
+                                        {
+
+                                            if (p.Id != me.Id
+                                                && p.ProcessName != "winlogon"
+                                                && p.ProcessName != "explorer"
+                                                && p.ProcessName != "System Idle Process"
+                                                && p.ProcessName != "taskmgr"
+                                                && p.ProcessName != "spoolsv"
+                                                && p.ProcessName != "csrss"
+                                                && p.ProcessName != "smss"
+                                                && p.ProcessName != "svchost "
+                                                && p.ProcessName != "services"                          
+                                                && p.ProcessName != processos[i]
+                                            )
+                                            {
+                                                p.PriorityClass = ProcessPriorityClass.Normal;
+                                                p.ProcessorAffinity = (IntPtr)0x000F;
+                                            }
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            //logging goes here
+                                            continue;
+                                        }
+                                    }
+
+                                    //  Process.Start(@"C:\Program Files (x86)\Steam\steamapps\common\CPUCores\cpucores.exe");
+                                    sim = 0;
+                                            ativar = 0;
+
+
+                                }
+                                }
+                            
+
+                            System.Threading.Thread.Sleep(5000);
+
+                        } while (sim > 0);
+
+
+                    }
+                    else
+                    {
+                        // start your process
+                    }
+                    System.Threading.Thread.Sleep(5000);
+
+                }
+
+
+
+            }
+
+
+
+        }
+
+        private void ShowWindow(object mainWindowHandle, int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   //reads the items that are in the txt file
+            //this.WindowState = FormWindowState.Minimized;
+
+            label1.ForeColor = System.Drawing.Color.Green;
+
+
+
+            go = System.AppDomain.CurrentDomain.BaseDirectory + "processos.txt";
+            //  System.Windows.Forms.MessageBox.Show(go);
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+          //  this.Size = Properties.Settings.Default.FormSize;
+            this.Text = "CPUCORES Fixer";
+
+            string[] lines = File.ReadAllLines(go);
+            listBox1.Items.AddRange(lines);
+
+
+            //counts and reads the processes to the background worker
+
+            do
+            {
+                processos[count] = listBox1.Items[count].ToString();
+
+                count = count + 1;
+            } while (count < listBox1.Items.Count);
+
+
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.RunWorkerAsync();
+            label1.Text = "Working...";
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {    //button to stop and start the backgroundworker
+            if (button2.Text == "STOP")
+            {
+
+
+                b = 1;
+                backgroundWorker1.CancelAsync();
+
+                button2.Text = "START";
+                label1.Text = "Stopped";
+                backgroundWorker1.CancelAsync();
+                foreach (Process p in Process.GetProcesses())
+                {
+                    try
+                    {
+                        if (p.ProcessName != "winlogon"
+                            && p.ProcessName != "explorer"
+                            && p.ProcessName != "System Idle Process"
+                            && p.ProcessName != "taskmgr"
+                            && p.ProcessName != "spoolsv"
+                            && p.ProcessName != "csrss"
+                            && p.ProcessName != "smss"
+                            && p.ProcessName != "svchost "
+                            && p.ProcessName != "services"
+                            && p.ProcessName != processos[i]
+                        )
+                        {
+                            p.PriorityClass = ProcessPriorityClass.Normal;
+                            p.ProcessorAffinity = (IntPtr)0x000F;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //logging goes here
+                        continue;
+                    }
+                }
+                label1.ForeColor = System.Drawing.Color.Red;
+
+
+            }
+            else
+            {
+                b = 2;
+
+                Application.Restart();
+
+
+
+
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   //adding items to the list box
+            listBox1.Items.Add(textBox1.Text);
+
+            //saving the list box items in the txt file
+            string sPath = go;
+
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach (var item in listBox1.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+
+            SaveFile.Close();
+
+
+
+
+
+            do
+            {
+                processos[count] = listBox1.Items[count].ToString();
+
+                count = count + 1;
+            } while (count < listBox1.Items.Count);
+
+            Application.Restart();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {   //removing items from listbox
+            listBox1.Items.Remove(listBox1.Items[listBox1.SelectedIndex]);
+
+            //saving to the txt file
+            string sPath = go;
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach (var item in listBox1.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+
+            SaveFile.Close();
+
+            Application.Restart();
+
+
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.FormSize = this.Size;
+            Properties.Settings.Default.Location = this.Location;
+            Properties.Settings.Default.Save();
+            foreach (Process p in Process.GetProcesses())
+            {
+                try
+                {
+                    if (p.ProcessName != "winlogon"
+                        && p.ProcessName != "explorer"
+                        && p.ProcessName != "System Idle Process"
+                        && p.ProcessName != "taskmgr"
+                        && p.ProcessName != "spoolsv"
+                        && p.ProcessName != "csrss"
+                        && p.ProcessName != "smss"
+                        && p.ProcessName != "svchost "
+                        && p.ProcessName != "services"
+                    )
+                    {
+                        p.PriorityClass = ProcessPriorityClass.Normal;
+                        p.ProcessorAffinity = (IntPtr)0x000F;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //logging goes here
+                    continue;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            //add
+            rk.SetValue("CpuCoresCsharpMELHORA", Application.ExecutablePath);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            //delete
+            rk.DeleteValue("CpuCoresCsharpMELHORA", false);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            /*    Process[] pa = Process.GetProcessesByName("Overwolf");
+
+
+                    foreach (Process process in pa)
+
+                    {
+                 process.ProcessorAffinity = (IntPtr)0x0002;
+           
+            }*/
+            backgroundWorker1.CancelAsync();
+
+            Process[] allProc = Process.GetProcesses();
+            Process me = Process.GetCurrentProcess();
+            foreach (Process p in Process.GetProcesses())
+            {
+                try
+                {
+                    if (p.Id != me.Id
+                        && p.ProcessName != "winlogon"
+                        && p.ProcessName != "explorer"
+                        && p.ProcessName != "System Idle Process"
+                        && p.ProcessName != "taskmgr"
+                        && p.ProcessName != "spoolsv"
+                        && p.ProcessName != "csrss"
+                        && p.ProcessName != "smss"
+                        && p.ProcessName != "svchost "
+                        && p.ProcessName != "services"
+                        && p.ProcessName != "chrome"
+                    )
+                    {
+                        p.PriorityClass = ProcessPriorityClass.BelowNormal;
+                        p.ProcessorAffinity = (IntPtr)0x0001;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //logging goes here
+                    continue;
+                }
+            }
+
+
+
+
+
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            //
+
+            }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            Process[] allProc = Process.GetProcesses();
+            Process me = Process.GetCurrentProcess();
+            foreach (Process p in Process.GetProcesses())
+            {
+                try
+                {
+                    if (p.Id != me.Id
+                        && p.ProcessName != "winlogon"
+                        && p.ProcessName != "explorer"
+                        && p.ProcessName != "System Idle Process"
+                        && p.ProcessName != "taskmgr.exe"
+                        && p.ProcessName != "spoolsv.exe"
+                        && p.ProcessName != "csrss.exe"
+                        && p.ProcessName != "smss.exe"
+                        && p.ProcessName != "svchost.exe "
+                        && p.ProcessName != "services.exe"                    
+                    )
+                    {
+                        p.PriorityClass = ProcessPriorityClass.Normal;
+                        p.ProcessorAffinity = (IntPtr)0x000F;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //logging goes here
+                    continue;
+                }
+            }
+
+        }
+    }
+    }
+
